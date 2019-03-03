76a354c20eb120e3193d89ebfe994bd25150aa09
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-03-03 23:46:33 +0530

    initial commit. android UI designs.

---
+++ app/src/main/java/com/industrialmaster/theflame/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.industrialmaster.theflame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void navigateToProfile(View view){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+}
